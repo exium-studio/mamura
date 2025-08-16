6c2f03e2a03e0722987e733ccc568c5d82ce1072
sulenq <fatwalinovera@gmail.com>
2025-08-16 13:41:31 +0700

    chores

--- src/pages/_admin/DashboardPage.tsx
+++
@@ -1,7 +0,0 @@
-import CContainer from "@/components/ui-custom/CContainer";
-
-const DashboardPage = () => {
-  return <CContainer p={4} gap={8}></CContainer>;
-};
-
-export default DashboardPage;
--- src/pages/_admin/ProfilePage.tsx
+++
@@ -1,7 +0,0 @@
-import CContainer from "@/components/ui-custom/CContainer";
-
-const ProfilePage = () => {
-  return <CContainer></CContainer>;
-};
-
-export default ProfilePage;

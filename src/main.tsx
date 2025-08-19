43cc31777135f3a32036d72b482d60335f9fe86c
sulenq <fatwalinovera@gmail.com>
2025-08-19 15:03:38 +0700

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { HelmetProvider } from "react-helmet-async";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     {/* <StrictMode> */}
-    {/* @ts-ignore */}
     <HelmetProvider>
       <Provider>
         <App />
